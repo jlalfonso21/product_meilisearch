53eeec1407d9d3397830cb4af16b5ba1a326a937
Jorge Luis Alfonso <jlalfonso21@gmail.com>
2022-03-23 10:31:49 -0400

    delegate preparing meilisearch args

--- controllers/main.py
+++ controllers/main.py
@@ -14,8 +14,7 @@ class ProductMeiliSearch(WebsiteSale):
         self.meili_product_index = None
         super().__init__()
     
-    @http.route('/shop/products/autocomplete', type='json', auth='public', website=True)
-    def products_autocomplete(self, term, options={}, **kwargs):
+    def _prepare_meili_search_args(self):
         if not self.meili_server_address:
             self.meili_server_address = request.env['ir.config_parameter'].sudo().get_param('msearch.service.url') or False 
             if not self.meili_server_address:
@@ -28,6 +27,10 @@ class ProductMeiliSearch(WebsiteSale):
             self.meili_product_index = request.env['ir.config_parameter'].sudo().get_param('msearch.service.product.index') or 'products' 
             if not self.meili_product_index:
                 raise ValidationError("Cant't get meilisearch server products index from database.")
+
+    @http.route('/shop/products/autocomplete', type='json', auth='public', website=True)
+    def products_autocomplete(self, term, options={}, **kwargs):
+        self._prepare_meili_search_args()
         
         res = super(ProductMeiliSearch, self).products_autocomplete(term, options, **kwargs)
         
