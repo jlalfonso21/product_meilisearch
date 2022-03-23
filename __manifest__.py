d71eedcbdb17d3a6f7cf6d3af84999d21f04c23a
Jorge Luis Alfonso <jlalfonso21@gmail.com>
2022-03-23 10:36:00 -0400

    reformat with black

--- __manifest__.py
+++ __manifest__.py
@@ -1,27 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Product Meilisearch Connector",
-
-    'summary': """
+    "name": "Product Meilisearch Connector",
+    "summary": """
         A simple addon to connect odoo product searching in the website with meilisearch
         """,
-
-    'description': """ ... """,
-
-    'author': "Jorge Luis Alfonso Chavez",
-
+    "description": """ ... """,
+    "author": "Jorge Luis Alfonso Chavez",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'website',
-    'version': '0.1',
-
+    "category": "website",
+    "version": "0.1",
     # any module necessary for this one to work correctly
-    'depends': ['website_sale', ],
-
+    "depends": [
+        "website_sale",
+    ],
     # always loaded
-    'data': [
+    "data": [
         # 'security/ir.model.access.csv',
-        'views/res_config_settings.xml',
+        "views/res_config_settings.xml",
     ],
 }
--- controllers/main.py
+++ controllers/main.py
@@ -7,32 +7,51 @@ from odoo.exceptions import ValidationError
 
 
 class ProductMeiliSearch(WebsiteSale):
-
     def __init__(self):
         self.meili_server_address = None
         self.meili_server_api_key = None
         self.meili_product_index = None
         super().__init__()
-    
+
     def _prepare_meili_search_args(self):
         if not self.meili_server_address:
-            self.meili_server_address = request.env['ir.config_parameter'].sudo().get_param('msearch.service.url') or False 
+            self.meili_server_address = (
+                request.env["ir.config_parameter"]
+                .sudo()
+                .get_param("msearch.service.url")
+                or False
+            )
             if not self.meili_server_address:
-                raise ValidationError("Cant't get meilisearch server address from database.")
+                raise ValidationError(
+                    "Cant't get meilisearch server address from database."
+                )
 
         if not self.meili_server_api_key:
-            self.meili_server_api_key = request.env['ir.config_parameter'].sudo().get_param('msearch.service.apikey') or False 
+            self.meili_server_api_key = (
+                request.env["ir.config_parameter"]
+                .sudo()
+                .get_param("msearch.service.apikey")
+                or False
+            )
 
         if not self.meili_product_index:
-            self.meili_product_index = request.env['ir.config_parameter'].sudo().get_param('msearch.service.product.index') or 'products' 
+            self.meili_product_index = (
+                request.env["ir.config_parameter"]
+                .sudo()
+                .get_param("msearch.service.product.index")
+                or "products"
+            )
             if not self.meili_product_index:
-                raise ValidationError("Cant't get meilisearch server products index from database.")
+                raise ValidationError(
+                    "Cant't get meilisearch server products index from database."
+                )
 
-    @http.route('/shop/products/autocomplete', type='json', auth='public', website=True)
+    @http.route("/shop/products/autocomplete", type="json", auth="public", website=True)
     def products_autocomplete(self, term, options={}, **kwargs):
         self._prepare_meili_search_args()
-        
-        res = super(ProductMeiliSearch, self).products_autocomplete(term, options, **kwargs)
-        
-        return res
 
+        res = super(ProductMeiliSearch, self).products_autocomplete(
+            term, options, **kwargs
+        )
+
+        return res
